c523514d1759a5a1c6b652dc14376bbaf2ce7bcf
Debora Morais <dependentwinter@gmail.com>
2025-04-01 21:26:14 -0300

    A eu ajeitei uns coisa
    tipo aquele negocio de digitar codigo quando abre e uns negocios ai adicionei para mudar fundo da pagina mt foda blz, ai também coloquei um segredo gigatonico

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
     var firstcomd = document.getElementById('firstcommand');
     var div2 = document.getElementById('cont-2');
     var pagina = document.getElementById('pagina');
-    var guestbook = document.getElementById('guestbook')
+    var guestbook = document.getElementById('guestbook');
+    var gbnamecolor = document.getElementById('gbnamecolor');
+    var inputdecorzinha = document.getElementById('nameColor');
 
     const commands = {
-        help: () => 'Comandos disponíveis: help, echo, date, clear',
+        help: () => '<b>Comandos disponíveis:</b> <br><b>help:</b> mostra comandos disponíveis.<br><b>echo:</b> echo "mensagem".<br> <b>date:</b> informa a data.<br><b>clear:</b> Faz a limpeza do terminal.<br><b>guestbook:</b> Incia o livro de visitas.<br><b>background:</b> o uso é: background "#064196". <br><b>helloworld:</b> --S-23-FK',
         sunfetch: () => {outputDiv.innerHTML = ''; div2.style.visibility = 'hidden'; div.style.visibility = 'visible'; firstcomd.style.visibility = 'visible';},
         echo: (args) => args.join(' '),
         date: () => new Date().toLocaleString(),
@@ -27,7 +29,8 @@ document.addEventListener('DOMContentLoaded', () => {
             div2.style.visibility = 'hidden'; // vai deixar a parte do console oculta.
             guestbook.style.visibility = 'visible'; //torna o guestbook visivel.
             gbname.focus(); //foco no nome
-        }
+        },
+        background: (args) => {document.body.style.background = args.join('')}
     };
 
     function setFocus() {
@@ -41,11 +44,70 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    //foco no nome
+    //foco no nome DO GUESTBOOK
     gbname.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
+            const negociodacor = {
+                cor: '#139CB7',
+            }; inputdecorzinha.value = negociodacor.cor;
             gbmessage.focus();
         }
+
+
+        //Nome Vermelho
+        
+        if (event.key === '!') {
+            gbnamecolor.style.color = '#a93226';
+            const negociodacor = {
+            cor: '#a93226',
+        }; inputdecorzinha.value = negociodacor.cor;}
+
+        //Nome Roxo
+
+        if (event.key === '@') {
+            gbnamecolor.style.color = '#7d3c98';
+            const negociodacor = {
+            cor: '#7d3c98',
+        }; inputdecorzinha.value = negociodacor.cor;}
+
+        
+        //Nome Azul
+
+        if (event.key === '#') {
+            gbnamecolor.style.color = '#2471a3';
+            const negociodacor = {
+            cor: '#2471a3',
+        }; inputdecorzinha.value = negociodacor.cor;}
+
+        
+        //Nome Verde
+
+        if (event.key === '$') {
+            gbnamecolor.style.color = '#138d75';
+            const negociodacor = {
+            cor: '#138d75',
+        }; inputdecorzinha.value = negociodacor.cor;}
+
+        //Nome Amarelo
+
+        if (event.key === '%') {
+            gbnamecolor.style.color = '#d4ac0d';
+            const negociodacor = {
+            cor: '#d4ac0d',
+        }; inputdecorzinha.value = negociodacor.cor;}    
+
+
+        //Nome Laranja
+
+        if (event.key === '&') {
+            gbnamecolor.style.color = '#ca6f1e';
+            const negociodacor = {
+            cor: '#ca6f1e',
+        }; inputdecorzinha.value = negociodacor.cor;}
+
+
+
+
     });
 
     // Quando o segundo input está vazio, o foco volta para o primeiro input
@@ -80,6 +142,17 @@ document.addEventListener('DOMContentLoaded', () => {
             div.style.visibility = 'hidden';
             firstcomd.style.visibility = 'hidden';
             div2.style.visibility = 'visible';
+        }
+
+        if (e.key === 'Enter') {
+            const commandLine = firstinput.value;
+            outputDiv.innerHTML += `<div>${commandLine}</div>`;
+            const result = executeCommand(commandLine);
+            if (result) {
+                outputDiv.innerHTML += `<div>${result}</div`;
+            }
+            firstinput.value = '';
+            outputDiv.scrollTop = outputDiv.scrollHeight;
         }
     })
 
